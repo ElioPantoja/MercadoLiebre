29ef8085eec7f45371e1f909535545209d443023
ElioPantoja <elio.pantoja@outlook.com>
2021-11-16 10:55:53 -0300

    copia del proyecto

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const app = express()
+const path = require("path")
+
+app.use(express.static(__dirname + "/public"))
+
+app.listen(process.env.PORT || 3030, () => {
+    console.log("ok")})
+
+app.get("/", (req, res) => 
+    res.sendFile(path.join(__dirname, "/views/home.html")))       
+app.get("/views/register", (req, res) => 
+    res.sendFile(path.join(__dirname, "/views/register.html")))   
+app.get("/views/login", (req, res) => 
+    res.sendFile(path.join(__dirname, "/views/login.html")))                      
